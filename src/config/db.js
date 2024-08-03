2fe1e67408ae1cc92612910f2a152096d63a8359
leonardobuckalves <leonardobuckalves@gmail.com>
2024-08-03 20:59:05 -0300

    Criando banco de dados e configurando variáveis"
    - Conexão com banco de dados
    - Criação do arquivo .env para uso de variáveis de ambiente
    - Conexão teste para validação da conexão inicial com o banco de dados

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 var pool = mysql.createPool({
-    "user": "root",
-    "password": "root",
-    "database": "gerenciadordespesas",
-    "host": "localhost",
-    "port": "3307"
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
--- src/routes/despesas.js
+++ src/routes/despesas.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const mysql = require('../config/db').pool;
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).send({
-        data: null,
-        sucess: true
+router.get('/teste', (req, res, next) => {
+    mysql.getConnection((error, connection) => {
+        if (error) {
+            return res.status(500).send({
+                data: error,
+                success: false
+            });
+        }
+        connection.query(`
+            INSERT INTO gerenciadordespesas.tipos_pagamento (tipo) 
+            VALUES ('Dinheiro'), ('Débito'), ('Crédito'), ('Pix')`,
+            (error, resultado) => {
+                connection.release();
+                if (error) {
+                    return res.status(500).send({
+                        data: error,
+                        sucess: false
+                    });
+                }
+                res.status(200).send({
+                    data: null,
+                    sucess: true
+                });
+            }
+        )
     });
 });
 
