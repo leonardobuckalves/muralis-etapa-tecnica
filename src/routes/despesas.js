e0bfb0a24106e333ca62a8f6477c7078bb619759
leonardobuckalves <leonardobuckalves@gmail.com>
2024-08-04 15:40:17 -0300

    feat: Rota Despesas GET e POST

--- src/routes/despesas.js
+++ src/routes/despesas.js
@@ -1,39 +1,51 @@
 const express = require('express');
 const mysql = require('../config/db').pool;
 const router = express.Router();
+const { format } = require('date-fns');
 
-router.get('/teste', (req, res, next) => {
+router.get('/', (req, res) => {
     mysql.getConnection((error, connection) => {
-        if (error) {
-            return res.status(500).send({
-                data: error,
-                success: false
-            });
-        }
-        connection.query(`
-            INSERT INTO gerenciadordespesas.tipos_pagamento (tipo) 
-            VALUES ('Dinheiro'), ('Débito'), ('Crédito'), ('Pix')`,
+        if (error) { return res.status(500).send({ data: error, success: false }) }
+        connection.query(
+            `SELECT * FROM despesas;`,
             (error, resultado) => {
-                connection.release();
                 if (error) {
-                    return res.status(500).send({
+                    return res.status(400).send({
                         data: error,
-                        sucess: false
+                        success: false
                     });
                 }
-                res.status(200).send({
-                    data: null,
-                    sucess: true
+                return res.status(200).send({
+                    data: resultado,
+                    success: true
                 });
             }
         )
     });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        data: "id",
-        sucess: true
+router.post('/', (req, res) => {
+    const dataFormatada = format(req.body.data_compra, 'yyyy-MM-dd HH:mm:ss');
+
+    mysql.getConnection((error, connection) => {
+        if (error) { return res.status(500).send({ data: error, success: false }) }
+        connection.query(
+            'INSERT INTO despesas (valor, data_compra, descricao, categorias_id, tipos_pagamento_id) VALUES (?, ?, ?, ?, ?)',
+            [req.body.valor, dataFormatada, req.body.descricao, req.body.categorias_id, req.body.tipos_pagamento_id],
+            (error, resultado) => {
+                connection.release();
+                if (error) {
+                    return res.status(400).send({
+                        data: error,
+                        sucess: false
+                    });
+                }
+                res.status(201).send({
+                    data: resultado.insertId,
+                    success: true
+                })
+            }
+        )
     });
 });
 
